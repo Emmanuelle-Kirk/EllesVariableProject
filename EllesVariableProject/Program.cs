a3c521e71593e6e0ba719a6bdfabd24f57092699
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-09-21 10:09:14 -0300

    RootinTootinLaserShootin
    Added Rootin. Added Tootin. Added Laser Shootin. Removed Tagline.

--- EllesVariableProject/Program.cs
+++ EllesVariableProject/Program.cs
@@ -12,7 +12,55 @@ namespace EllesVariableProject
         {
 
             Console.WriteLine("Elle presents...");
-            Console.WriteLine("THE ONLY GAME YOU EVER NEED TO PLAY!!!")
+            Console.WriteLine("Rootin' Tootin' Laser Shootin'");
+
+            int lives;
+
+            string name;
+            name = "Barnaby"; //Player Name
+            lives = 3;
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("You are a space cowboy.");
+
+
+            float weaponHeat;
+            float weaponDischarge;
+
+            weaponHeat = 50.00f;
+            Console.WriteLine("Weapon Heat: " + weaponHeat);
+            weaponDischarge = 0.5f;
+            Console.WriteLine("WEAPON FIRED!");
+            Console.WriteLine("Weapon Heat: " + weaponHeat / weaponDischarge); //Weapon heats up from being fired
+
+            int score;
+            int points;
+
+            points = 100;
+            score = 0 + points; //Score goes up by the amount of points earned
+            Console.WriteLine("Score: " + score);
+
+            int multiplier;
+            multiplier = 3; //Combo Multiplier
+
+            Console.WriteLine("Three kill streak!");
+
+            Console.WriteLine("Combo Achived!");
+            Console.WriteLine("Score: " + points * multiplier);
+            score = 0 + points * multiplier;
+            Console.WriteLine("Four kill streak!");
+            Console.WriteLine("Combo Achived!");
+            Console.WriteLine("Score: " + score * multiplier);
+
+            Console.WriteLine("YOU DIED");
+            score = 0;
+            lives = lives - 1;
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Lives: " + lives);
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
