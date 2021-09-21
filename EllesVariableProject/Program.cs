86e4e1cbed6f7daae4187063e7c8242ddb02c4d3
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-09-21 09:27:52 -0300

    Initial commit

---
+++ EllesVariableProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EllesVariableProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Elle presents...");
+            Console.WriteLine("THE ONLY GAME YOU EVER NEED TO PLAY!!!")
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EllesVariableProject
+ 
